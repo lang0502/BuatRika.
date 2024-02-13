bc2d3108d5e93a21c57b07ad127bbc7e431136ec
lang0502 <153420127+lang0502@users.noreply.github.com>
2024-02-14 03:23:33 +0700

    Add files via upload

---
+++ lang/README.js
@@ -0,0 +1,9 @@
+var popupconfirm = confirm("Jika Menggunakan Web HandPhone , Miringkan Layar HP Anda Biar Tampilan Full dan Rapi")
+if(popupconfirm === true)
+{
+       alert("Abaikan JIka Menggunakan Browser Laptop/Computer");
+}
+else{
+       alert("Maaf Jika Tampilan tidak Sempurna");
+
+}
